70610e2845abbeab2a24b7d7d4e25d97885b5ec2
Niisar <nisar2048@gmail.com>
2015-04-23 20:19:55 +0530

    first wcf hello world

---
+++ Basic/GettingStarted/ConsoleClient/Service1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Service1
+{
+    class Program
+    {
+        [System.ServiceModel.ServiceContractAttribute(Namespace = "MyService", ConfigurationName = "service.ICalculator")]
+        public interface ICalculator
+        {
+            [System.ServiceModel.OperationContractAttribute(Action = "MyService/ICalculator/Add", ReplyAction = "MyService/ICalculator/AddResponse")]
+            double Add(double n1, double n2);
+
+            [System.ServiceModel.OperationContractAttribute(Action = "MyService/ICalculator/Substract", ReplyAction = "MyService/ICalculator/SubstractResponse")]
+            double Substract(double n1, double n2);
+
+            [System.ServiceModel.OperationContractAttribute(Action = "MyService/ICalculator/Multiply", ReplyAction = "MyService/ICalculator/MultiplyResponse")]
+            double Multiply(double n1, double n2);
+
+            [System.ServiceModel.OperationContractAttribute(Action = "MyService/ICalculator/Devide", ReplyAction = "MyService/ICalculator/DevideResponse")]
+            double Devide(double n1, double n2);
+        }
+
+        public interface ICalculatorChannel : ICalculator, IClientChannel
+        {
+        }
+
+        public partial class CalculatorClient :ClientBase<ICalculator>, ICalculator
+        {
+            public CalculatorClient()
+            {
+            }
+
+            public CalculatorClient(string endpointConfigurationName) :
+                base(endpointConfigurationName)
+            {
+            }
+
+            public CalculatorClient(string endpointConfigurationName, string remoteAddress) :
+                base(endpointConfigurationName, remoteAddress)
+            {
+            }
+
+            public CalculatorClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+                base(endpointConfigurationName, remoteAddress)
+            {
+            }
+
+            public CalculatorClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+                base(binding, remoteAddress)
+            {
+            }
+            public double Add(double n1, double n2)
+            {
+                return base.Channel.Add(n1, n2);
+            }
+
+            public double Substract(double n1, double n2)
+            {
+                throw new NotImplementedException();
+            }
+
+            public double Multiply(double n1, double n2)
+            {
+                throw new NotImplementedException();
+            }
+
+            public double Devide(double n1, double n2)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CalculatorClient client = new CalculatorClient();
+            Console.WriteLine(client.Add(20, 21));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic/GettingStarted/service/service/IService1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace service
+{
+    [ServiceContract(Namespace = "MyService")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Substract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Devide(double n1, double n2);
+    }
+     
+
+
+
+}
